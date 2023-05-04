fbdda581c862fc981026ff8d02c3be691699b46f
iCODEalien <dixit.b.c@gmail.com>
2023-05-04 15:25:05 +0530

    Code changes 3:25

---
+++ func.py
@@ -0,0 +1,10 @@
+def print_dict(di):
+    for key in di.keys():
+        print(di[key])
+
+def print_list(li):
+    for element in li:
+        print(element)
+
+def print_tuple(tu):
+def print_string(st):
